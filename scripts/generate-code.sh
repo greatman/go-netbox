f59b7423d2179348186838ce9b41f251501b2dc2
William Lebel <lebelwilliam@greatmancode.com>
2024-10-04 18:56:14 -0400

    Update to 4.1.3

--- scripts/generate-code.sh
+++ scripts/generate-code.sh
@@ -8,10 +8,4 @@ for F in $(cat .openapi-generator/files) ; do
 done
 
 # Generate library
-docker run --rm --env JAVA_OPTS=-DmaxYamlCodePoints=9999999 -v "${PWD}:/local" openapitools/openapi-generator-cli:v7.6.0 \
-    generate \
-    --config /local/.openapi-generator/config.yaml \
-    --input-spec /local/api/openapi.yaml \
-    --output /local \
-    --inline-schema-options RESOLVE_INLINE_ENUMS=true \
-    --http-user-agent go-netbox/$(cat api/netbox_version)
+docker run --rm --env JAVA_OPTS=-DmaxYamlCodePoints=9999999 -v "${PWD}:/local" openapitools/openapi-generator-cli:v7.8.0 generate --config /local/.openapi-generator/config.yaml --input-spec /local/api/openapi.yaml --output /local --inline-schema-options RESOLVE_INLINE_ENUMS=true --http-user-agent go-netbox/4.1.3
